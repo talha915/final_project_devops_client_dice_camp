0f7d0aa894900eada296bb04a00730164bedd4ae
talha915 <talhazafar915426@gmail.com>
2024-03-03 22:21:31 +0100

    file checksum integrity passed

--- client.js
+++ client.js
@@ -2,20 +2,48 @@ const axios = require('axios');
 const fs = require('fs');
 const crypto = require('crypto');
 
-// Replace 'http://your-server-url:8000/generate_file' with the actual URL of your server
+
 const serverUrl = 'http://localhost:8000/generate_file';
 
-const res = axios.get(serverUrl);
-console.log("Res: ", res)
 
-const get_data=async()=> {
+
+const downloadFile = async () => {
     try {
-        const res = await axios.get(serverUrl);
-        console.log("Res: ", res.data);
+      const response = await axios.get(serverUrl, { responseType: 'arraybuffer', maxContentLength: 5000000 });
+  
+      const filePath = './clientdata/received_file.txt';
+  
+      fs.writeFileSync(filePath, response.data);
+      console.log(`File saved successfully at: ${filePath}`);
+      console.log("Res: ", response.headers.checksum);
+      checksum(filePath, response.data, response.headers.checksum)
+    } catch (error) {
+      console.error('Error downloading file:', error.message);
+    }
+};
+
+const checksum = async(filePath, response, checksum)=> {
+    try {
+        const fileContent = fs.readFileSync(filePath);
+        const receivedChecksum = crypto.createHash('sha256').update(fileContent).digest('hex');
+
+        // Verify the checksum received from the server
+        console.log(`Received Checksum: ${checksum}`);
+        console.log(`Calculated Checksum: ${receivedChecksum}`);
+
+        if (checksum === receivedChecksum) {
+            console.log('Checksum verification passed. File integrity is verified.');
+        } else {
+            console.error('Checksum verification failed. File integrity cannot be guaranteed.');
+        }
     }
     catch (error) {
-        console.log("Error: ", res);
+        console.error('Error checksum file:', error.message);
     }
 }
 
-get_data();
+downloadFile();
+
+
+
+
